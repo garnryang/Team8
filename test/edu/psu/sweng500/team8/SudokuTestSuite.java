44feb0c1c1c898856f89bc9793bc43f965b525d7
Scott <garnryang@gmail.com>
2015-06-07 19:24:43 -0400

    SudokuGUI file is added form file is in Resources folder

--- test/edu/psu/sweng500/team8/SudokuTestSuite.java
+++ test/edu/psu/sweng500/team8/SudokuTestSuite.java
@@ -10,7 +10,11 @@ import edu.psu.sweng500.team8.coreDataStructures.CellGridTest;
 import edu.psu.sweng500.team8.coreDataStructures.CellTest;
 import edu.psu.sweng500.team8.coreDataStructures.PencilMarkManagerTests;
 import edu.psu.sweng500.team8.play.GameSessionTests;
+import edu.psu.sweng500.team8.puzzleGenerator.FillPuzzleTests;
+import edu.psu.sweng500.team8.puzzleGenerator.GameTests;
+import edu.psu.sweng500.team8.puzzleGenerator.PuzzleTests;
 import edu.psu.sweng500.team8.puzzleGenerator.SolutionGeneratorTest;
+import edu.psu.sweng500.team8.puzzleGenerator.Test5;
 import edu.psu.sweng500.team8.solver.ConstraintSolverTests;
 import edu.psu.sweng500.team8.solver.GuessAndCheckSolverTests;
 import edu.psu.sweng500.team8.solver.HintGeneratorTests;
@@ -29,7 +33,11 @@ import edu.psu.sweng500.team8.solver.HintGeneratorTests;
    ConstraintSolverTests.class,
    GuessAndCheckSolverTests.class,
    HintGeneratorTests.class,
-   GameSessionTests.class
+   GameSessionTests.class,
+   Test5.class,
+   FillPuzzleTests.class,
+   GameTests.class,
+   PuzzleTests.class
 })
 public class SudokuTestSuite {
 
